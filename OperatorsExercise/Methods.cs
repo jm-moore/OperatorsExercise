52fb8380165ab3d6ca60807b1e253779779d3576
jm-moore <jmmoore9358@gmail.com>
2022-08-09 14:11:51 -0500

    initial commit

---
+++ OperatorsExercise/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsExercise
+{
+    public class Methods
+    {
+        public static void QuotientRemain()
+        {
+            Console.WriteLine("Give me a number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give me another number:");
+            int b = int.Parse(Console.ReadLine());
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == a && b == b)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+        }
+    }
+}
